af88ba519c7672dcce6416b0d904d734aaaf7dbf
BCA-krishna <krishnapatel1284@gmail.com>
2026-08-09 18:32:15 +0530

    Fix backend Render URL

--- shopsphere-frontend/src/api/axiosConfig.js
+++ shopsphere-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://shopsphere-backend-w77.onrender.com/api",
+    baseURL: "https://shopsphere-backend-vw77.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
